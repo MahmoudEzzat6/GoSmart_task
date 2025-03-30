6deda740fcec3285ecbfabdbb29e780bc63b4b2d
Mahmoud Ezzat <72379590+MahmoudEzzat6@users.noreply.github.com>
2025-03-30 11:49:55 +0200

    handel_data

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyApp extends StatelessWidget {
         providers: [
           BlocProvider(
               create: (BuildContext context) =>HomeCubit()..fetchData()),
-//git remote add origin https://github.com/MahmoudEzzat6/GoSmartTask.git
           BlocProvider(
               create: (BuildContext context) =>LoginCubit()),
           ],
